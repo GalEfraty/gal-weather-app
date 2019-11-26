69c6fcbae8f60d591d7ccbdb41625e5be7b2064b
Gal Efraty <gal.efraty@gmail.com>
2019-11-26 21:04:50 +0200

    add authentication process

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,80 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import logo from "../img/logo4.png";
 import "../styles/styles.css";
 
+import userContext from "../context/userContext";
+
 const Header = () => {
+  const { loggedInUser } = useContext(userContext);
+  useEffect(() => {
+    if (loggedInUser) {
+      console.log(loggedInUser);
+    }
+  });
+
+  const renderHeaderContent = () => {
+    let headerContent = "";
+    if (loggedInUser) {
+      headerContent = (
+        <ul className="navbar-nav ml-auto flex-nowrap">
+          <li className="nav-item">
+            <a href="/" className="nav-link">
+              <span className="fas fa-heart"></span>
+              {"  "}
+              Hi {loggedInUser.fullName}
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/api/logout" className="nav-link">
+              <span className="fas fa-sign-out-alt"></span>
+              {"  "}
+              logout
+            </a>
+          </li>
+        </ul>
+      );
+    } else {
+      //fas fa-sign-in-alt
+      headerContent = (
+        <ul className="navbar-nav ml-auto flex-nowrap">
+          <li className="nav-item">
+            <a href="/auth/google" className="nav-link">
+              <span className="fab fa-google"></span>
+              {"  "}
+              Login
+            </a>
+          </li>
+        </ul>
+      );
+    }
+
+    return headerContent;
+  };
+
   return (
-    <nav className="navbar navbar-dark bg-primary">
-      <ul className="navbar-nav">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="d-flex flex-grow-1">
+        <span className="w-100 d-lg-none d-block"></span>
         <a className="navbar-brand" href="/">
           <img src={logo} width="40" height="40" alt="Logo" />
         </a>
-      </ul>
-
-      <ul className="navbar-nav">
-        <li className="nav-item active">
-          <a className="nav-link" href="/">
-            Hello!
-          </a>
-        </li>
-      </ul>
+        <div className="w-100 text-right">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#myNavbar7"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div>
+      <div
+        className="collapse navbar-collapse flex-grow-1 text-right"
+        id="myNavbar7"
+      >
+        {renderHeaderContent()}
+      </div>
     </nav>
   );
 };
--- client/src/components/WeatherApp.js
+++ client/src/components/WeatherApp.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import axios from "axios";
 import "../styles/styles.css";
 import WeatherDashboard from "./WeatherDashboard";
 import Header from "./Header";
 
+import userContext from "../context/userContext";
+
 const WeatherApp = () => {
+  const [loggedInUser, setLoggedInUser] = useState("");
+
+  const fetchUser = async () => {
+    console.log("fetching user");
+    const fetchedUser = await axios.get("/api/current_user")
+    console.log("fetched user: ", fetchedUser)
+    setLoggedInUser(fetchedUser.data)
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+
   return (
     <div className="App">
-      <Header />
-      <BrowserRouter>
-        <Route path={"/"} component={WeatherDashboard} exact />
-      </BrowserRouter>
+      <userContext.Provider value={{loggedInUser}}>
+        <Header />
+        <BrowserRouter>
+          <Route path={"/"} component={WeatherDashboard} exact />
+        </BrowserRouter>
+      </userContext.Provider>
     </div>
   );
 };
